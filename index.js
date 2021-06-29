d46da5fde5a7ec578ad258c804b6b3e068b9106e
Stephen Arancio <sjarancio@gmail.com>
2021-06-29 11:16:46 -0400

    commit tree 4365fd4475b287848228603c8433f5e630a30ca0

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+function saturdayFun(act="roller-skate"){
+    return `This Saturday, I want to ${act}!`
+}
+
+
+
+const mondayWork = function(work="go to the office"){
+    return `This Monday, I will ${work}.`
+}
+
+
+function wrapAdjective(str="*"){
+    return function(defa="special"){
+        return `You are ${str}${defa}${str}!`
+    }
+}
+
+// wrapAdjective("%")("a dedicated programmer");
